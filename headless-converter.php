e77b4cbe70997e23fa657e9f4f652d684c1179f7
Atte Liimatainen <atte.liimat@gmail.com>
2021-11-15 14:58:11 +0200

    bump version

--- headless-converter.php
+++ headless-converter.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Headless Converter
  * Plugin URI:        https://github.com/AttLii/headless-converter
  * Description:       Converts frontend to JSON response when request is done with certain conditions.
- * Version:           1.0.2
+ * Version:           1.0.3
  * Requires at least: 5.6
  * Requires PHP:      7.4
  * Author:            Atte Liimatainen
